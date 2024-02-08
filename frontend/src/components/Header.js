81327a83e1983ad310c74e79e1d99db4f28c2186
guigalde <guillermo.galeanodepaz@gmail.com>
2024-02-08 15:57:08 +0100

    Fix: Frontend project moved out of main folder #1

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,10 @@
+import * as react from 'react';
+
+export default function Header(props){
+    return (
+        <header>
+            <img src={props.logoSrc} alt ="logo"/>
+            <h1>{props.pageTitle}</h1>
+        </header>
+    );
+}
---
+++ src/main/resources/db/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO roles VALUES(1,'admin');
+INSERT INTO roles VALUES(2,'user');
+INSERT INTO users (id, username, password, email, role_id) VALUES(1,'admin','admin','admin@gmail.com',1);
